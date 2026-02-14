1a9fe53dcc83a1d46fd3d331931cdf86c3f7ff4c
JulianKonowalski <julian.konwalski@gmail.com>
2026-02-14 14:14:54 +0100

    add glfw and glad

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,24 @@
-#include <iostream>
+#include <glad/glad.h>
+#include <glfw/glfw3.h>
 
 int main(void) {
-    std::cout << "The day has come, I have to do the doings..." << std::endl;
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(300, 300, "Test Window", nullptr, nullptr);
+
+    glfwMakeContextCurrent(window);
+    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+    
+    while(!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        glViewport(0, 0, 200, 300);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(0, 0, 0, 1);
+
+        glfwSwapBuffers(window);
+    }
 }
