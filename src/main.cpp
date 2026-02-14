0e6041b1404b87f614a814e2527de4ab26781ade
JulianKonowalski <julian.konwalski@gmail.com>
2026-02-14 14:21:04 +0100

    add salad

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,9 @@
-#include <glad/glad.h>
-#include <glfw/glfw3.h>
+#include <AL/al.h>
+#include <AL/alc.h>
+#include <AL/salad.h>
 
 int main(void) {
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-    GLFWwindow* window = glfwCreateWindow(300, 300, "Test Window", nullptr, nullptr);
-
-    glfwMakeContextCurrent(window);
-    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
-    
-    while(!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
-
-        glViewport(0, 0, 200, 300);
-        glClear(GL_COLOR_BUFFER_BIT);
-        glClearColor(0, 0, 0, 1);
-
-        glfwSwapBuffers(window);
-    }
+    saladLoadALdefault();
+    ALCdevice* device = alcOpenDevice(nullptr);
+    return 0;
 }
